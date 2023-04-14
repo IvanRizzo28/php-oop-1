34ea1239a97445ae926b68fee80d8af4721dff4a
IvanRizzo28 <22ivanrizzo@gmail.com>
2023-04-14 15:36:03 +0200

    first commit

---
+++ Movie.php
@@ -0,0 +1,47 @@
+<?php
+
+class Movie{
+    private string $nome;
+    private int $durata;
+    private int $voto;
+    private string $genere;
+
+    public function __construct(string $nome,int $durata, int $voto, string $genere){
+        $this->nome=$nome;
+        $this->durata=$durata;
+        $this->voto=$voto;
+        $this->genere=$genere;
+    }
+
+    public function getNome(){
+        return $nome;
+    }
+
+    public function setNome($nome){
+        $this->nome=$nome;
+    }
+
+    public function getDurata(){
+        return $durata;
+    }
+
+    public function setDurata($durata){
+        $this->durata=$durata;
+    }
+
+    public function getVoto(){
+        return $voto;
+    }
+
+    public function setVoto($voto){
+        $this->voto=$voto;
+    }
+
+    public function getGenere(){
+        return $genere;
+    }
+
+    public function setGenere($genere){
+        $this->genere=$genere;
+    }
+}
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__.'/Movie.php';
+var_dump(new Movie("pierino",148,5,"commedia"));
+var_dump(new Movie("boh",148,5,"prova"));
